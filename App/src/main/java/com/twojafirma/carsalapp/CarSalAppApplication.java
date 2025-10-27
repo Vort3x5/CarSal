8742b6f0773d4f48c106f59b4e7cda7ef5ffc27b
vortex <Faking.Games857@gmail.com>
2025-10-27 13:13:18 +0100

    spring app generated

---
+++ App/src/main/java/com/twojafirma/carsalapp/CarSalAppApplication.java
@@ -0,0 +1,13 @@
+package com.twojafirma.carsalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSalAppApplication.class, args);
+	}
+
+}
---
+++ App/src/test/java/com/twojafirma/carsalapp/CarSalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twojafirma.carsalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
